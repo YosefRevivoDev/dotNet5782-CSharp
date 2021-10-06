ed945a522fe4be9106c5e90ba97282646b50bf8b
TomerZecharia <tzechari@g.jct.ac.il>
2021-10-06 16:09:13 +0300

    change

---
+++ dotNet5782_2406_1836/Targil_0/Program1836.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil_0
+{
+    partial class Program
+    {
+        static partial void Welcome1836()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
