3c8bb551b5dd6976463ff2eda26788130eae9434
Yosef Revivo <yosef_revivo@outlook.co.il>
2021-10-06 15:32:26 +0300

    initial code+Welcome function

---
+++ dotNet5782_2406_1836/Targil_0/Program2406.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Targil_0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2406();
+            Welcome1836();
+        }
+        static partial void Welcome1836();
+        private static void Welcome2406()
+        {
+            Console.WriteLine("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine(username + ", welcome to my first console application");
+        }
+    }
+}
