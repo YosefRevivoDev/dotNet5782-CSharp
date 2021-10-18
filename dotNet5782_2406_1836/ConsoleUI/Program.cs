88771cd4d5c1cc8ce9e8664c835c68509c98f08a
Yosef Revivo <yosef_revivo@outlook.co.il>
2021-10-18 23:38:39 +0300

    started project

---
+++ dotNet5782_2406_1836/ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DAL;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            DAL.StartApplication apps = new StartApplication();
+            apps.start();
+
+        }
+    }
+}
---
+++ dotNet5782_2406_1836/DAL/StartApplication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class StartApplication
+    {
+        public void start()
+        {
+            CHOICE choice;
+            DateTime Applicationstarted = DateTime.Now;
+            do
+            {
+                Console.WriteLine("Select your choice: ");
+                bool correct = Enum.TryParse(Console.ReadLine(), out choice);
+                if (!correct)
+                {
+                    continue;
+                }
+                switch (choice)
+                {
+                    //choose 0 
+                    case CHOICE.ADD:
+                        //TimeSpan span = DateTime.Now - Applicationstarted;
+
+                        break;
+                    case CHOICE.UPDATE:
+                        break;
+                    case CHOICE.DISPLAY:
+                        break;
+                    case CHOICE.VIEW_LISTS:
+                        break;
+                    case CHOICE.EXIT:
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != CHOICE.EXIT);
+        }
+    }
+}
