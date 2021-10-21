8d7ce4681d474d2d1f951024843db0229efa2aa8
TomerZecharia <tzechari@g.jct.ac.il>
2021-10-21 11:22:28 +0300

    parcel update

--- dotNet5782_2406_1836/DAL/IDAL.cs
+++ dotNet5782_2406_1836/DAL/IDAL.cs
@@ -5,20 +5,8 @@ namespace IDAL
 {
     namespace DO
     {
-        public struct Parcel
-        {
-            public int Id { get; set; }
-            public int SenderId { get; set; }
-            public int TargetId { get; set; }
-            public WeightCategories Parcel_weight { get; internal set; }
-            public priority Parcel_priority { get; internal set; }
-            public DateTime intvation_date { get; internal set; }
-            public int Dronled  { get; set; }
-            public DateTime Schueduled { get; internal set; }
-            public DateTime PickedUp { get; internal set; }
-            public DateTime Delivered { get; internal set; }
+       
 
-
-        }
+        
     }
 }
